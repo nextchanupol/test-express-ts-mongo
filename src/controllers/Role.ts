0a83940fb244c0d4aabcf75f2884d2e37e08e7b3
Ouu Naja <nextchanupol@gmail.com>
2022-05-29 11:19:03 +0700

    role controller

--- src/controllers/Role.ts
+++ src/controllers/Role.ts
@@ -1,8 +1,33 @@
-import { Request, Response, NextFunction } from 'express';
+import e, { Request, Response, NextFunction } from 'express';
 import mongoose from 'mongoose';
 import Role from '../models/Role';
 
-const get = (req: Request, res: Response, next: NextFunction) => {};
-const list = (req: Request, res: Response, next: NextFunction) => {};
-const create = (req: Request, res: Response, next: NextFunction) => {};
+const get = (req: Request, res: Response, next: NextFunction) => {
+    const { roleID } = req.params;
+
+    return Role.findById(roleID)
+        .then((role) => (role ? res.status(200).json(role) : res.status(404).json({ message: 'Not found' })))
+        .catch((error) => res.status(500).json({ error }));
+};
+
+const list = (req: Request, res: Response, next: NextFunction) => {
+    return Role.find()
+        .then((roles) => res.status(200).json(roles))
+        .catch((error) => res.status(500).json({ error }));
+};
+
+const create = (req: Request, res: Response, next: NextFunction) => {
+    const { name } = req.body;
+
+    const role = new Role({
+        _id: new mongoose.Types.ObjectId(),
+        name,
+    });
+
+    return role
+        .save()
+        .then((role) => res.status(201).json({ role }))
+        .catch((error) => res.status(500).json({ error }));
+};
 const update = (req: Request, res: Response, next: NextFunction) => {};
+const del = (req: Request, res: Response, next: NextFunction) => {};
